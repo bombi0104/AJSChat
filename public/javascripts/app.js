0923f230d6293f1ccbab24189249084805b3a61a
bombi0104 <bombi0104@gmail.com>
2015-05-20 00:48:13 +0700

    Init source

---
+++ public/javascripts/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('ChatApp', []);
+
+app.controller('MainCtrl', function($scope, $http) {
+  $scope.name = 'World';
+  $http.get('http://localhost:3000/groups.json').
+        success(function(data) {
+            $scope.groups = data;
+        });
+
+        $http.get('http://localhost:3000/messages.json').
+        success(function(data) {
+            $scope.messages = data;
+        });
+});
