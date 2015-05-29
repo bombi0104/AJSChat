edc3aeb2d06025ce1f3af1546109edea67c4bf12
taipa <taipa@nec.vn>
2015-05-29 16:30:35 +0700

    Add notification

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,6 +5,8 @@ app.controller('MainCtrl', function($scope, $http) {
 	$scope.name = null;
     $scope.token = null;
     $scope.currentGroup = null;
+    $scope.currentGroupIndex = 0;
+    $scope.groups = [];
 
     $scope.login = function() {
         var jsonData = {
@@ -30,9 +32,11 @@ app.controller('MainCtrl', function($scope, $http) {
                 // live-updates
                 var chatEvents = new EventSource('/stream/' + data._id);
                 chatEvents.addEventListener("chat", function(event) {
-                    var chat = JSON.parse(event.data);
-                    console.log(chat);
-                    receiveMsg(chat);
+                    $scope.$apply(function(){
+                        var chat = JSON.parse(event.data);
+                        console.log(chat);
+                        receiveMsg(chat);    
+                    });
                 });
                 
             })
@@ -51,26 +55,24 @@ app.controller('MainCtrl', function($scope, $http) {
         // $scope.Logs = room.chats;
 
         // Update selected row.
-        $scope.groups.forEach(function(r) {
-            if (r._id != group._id) {
-                r.active = false;
+        for (var i = $scope.groups.length - 1; i >= 0; i--) {
+            if ($scope.groups[i]._id != group._id) {
+                $scope.groups[i].active = false;
+                console.log("Select log : ", i);
             } else {
-                r.active = true;
+                $scope.currentGroupIndex = i;
+                $scope.groups[i].active = true;
+                $scope.groups[i].unread = 0;        // Reset unread message no.
             }
-        });
+        };
 
         if (group.messages == null){
             $http.get('/messages/group/' + group._id).
                 success(function(data) {
                     group.messages = data;
-                    $scope.messages = group.messages;
                 }
-            );    
-        } else {
-            $scope.messages = group.messages;
+            );
         }
-
-    	
 	};
 
     $scope.sendMsg = function(e) {
@@ -90,17 +92,40 @@ app.controller('MainCtrl', function($scope, $http) {
                 .success(function(data) {
                     // Reset inputed message
                     $scope.inputMsg = "";
-                    $scope.messages.push(data);
+                    //$scope.messages.push(data);
+                    receiveMsg(data);
                 }
             );
         }
     };
 
     var receiveMsg = function(msg){
-        $scope.groups.forEach(function(r) {
-            if (r._id == msg.group) {
-                r.messages.push(msg);
-                r.unread = r.unread + 1;
+        $scope.groups.forEach(function(group) {
+            if (group._id == msg.group) {
+                if (group.messages != null) {
+                    var isExist = false;
+                    group.messages.forEach(function(m){
+                        if (m._id == msg._id)
+                            isExist = true;
+                    });
+                    if (!isExist){
+                        group.messages.push(msg);
+
+                        // Just add new message count in case not showed group.
+                        if (group._id != $scope.groups[$scope.currentGroupIndex]._id){
+                            group.unread = group.unread + 1;
+                            notifyMe("From : " + msg.from_user.name + " \n " + msg.content);
+
+                        }
+                    }
+                } else {
+                    $http.get('/messages/group/' + group._id).
+                        success(function(data) {
+                            group.messages = data;
+                            group.unread = group.unread + 1;
+                        }
+                    );
+                }
             }
         });
     }
@@ -109,5 +134,34 @@ app.controller('MainCtrl', function($scope, $http) {
         $scope.groups.forEach(function(r) {
             r.unread = 0;
         });
+    }
+
+    var notifyMe = function(msg){
+        // Let's check if the browser supports notifications
+        if (!("Notification" in window)) {
+            alert("This browser does not support desktop notification");
+        }
+        else if (Notification.permission === "granted") {
+            // If it's okay let's create a notification
+            var notification = new Notification(msg);
+        }
+
+          // Otherwise, we need to ask the user for permission
+          // Note, Chrome does not implement the permission static property
+          // So we have to check for NOT 'denied' instead of 'default'
+          else if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function (permission) {
+
+              // Whatever the user answers, we make sure we store the information
+              if(!('permission' in Notification)) {
+                Notification.permission = permission;
+              }
+
+              // If the user is okay, let's create a notification
+              if (permission === "granted") {
+                var notification = new Notification(msg);
+              }
+          });
+        }
     }
 });
